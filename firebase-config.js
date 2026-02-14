47e44b3a310ba21c3d5e7ca2765ebae440842442
Alison Cardoso <alisoncardoso@icloud.com>
2026-02-14 16:54:46 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+/**
+ * Configuração do Firebase
+ * 1. Copie este arquivo e renomeie para: firebase-config.js
+ * 2. Crie um projeto em https://console.firebase.google.com
+ * 3. Ative Authentication (Google) e Firestore
+ * 4. Em Configurações do projeto > Seus apps > Web, pegue o objeto firebaseConfig
+ * 5. Cole abaixo substituindo os valores
+ * Não commite firebase-config.js (ele está no .gitignore)
+ */
+window.firebaseConfig = {
+  apiKey: "AIzaSyATc5IHhUuvv5_Dh-f1l12Iyche2u9bn9A",
+  authDomain: "egg-timer-c14f1.firebaseapp.com",
+  projectId: "egg-timer-c14f1",
+  storageBucket: "egg-timer-c14f1.firebasestorage.app",
+  messagingSenderId: "298945280693",
+  appId: "1:298945280693:web:f72d58156abcf8b6f7dcfc"
+};
